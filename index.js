9233004b9a31e15f3444e584ee29739371ce9845
pato091 <patriciososa91@gmail.com>
2021-06-21 19:31:16 -0300

    remove host form index.js

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ app.use('/',routes());
 
 
 //servidor y puerto
-const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000;
+// const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
 
 app.listen(port,host, ()=>{
     console.log('El servidor esta funcionando');
